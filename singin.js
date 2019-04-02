450997e4bd295d3b253dfa03b353319c53f7be2a
JaquelineGDS <jaqueline_gd@outlook.com>
2019-04-02 14:50:35 -0300

    alteraçoes css, javascript, login

--- singin.js
+++ singin.js
@@ -1,29 +1,90 @@
 $(document).ready(function(){
    
-     $(".btnSingnIn").click(function(event){
+     $(".btn-singn-in").click(function(event){
        
         event.preventDefault();
 
-        let inputEmail = $(".InputEmail1").val();
-        let inputPassword = $(".InputPassword1").val();    
+        let email = $(".input-email").val();
+        let password = $(".input-password").val();    
         
          console.log("AQUI")
-         console.log(inputEmail)
-         console.log(inputPassword)
+         console.log(email)
+         console.log(password)
          
         firebase
             .auth()
-            .signInWithEmailAndPassword(inputEmail, inputPassword)
-            .then(function(){
-                alert('Bem vindo '+ InputEmail1);
+            .signInWithEmailAndPassword(email, password)
+            .then(function(response){
+                console.log(response)
+                var uid = response.user.uid;
+                //window.location = "home.html?userId=" + uid;
+                window.location.assign("home.html?userId=" + uid);
             })
             .catch(function(error) {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.error(errorCode);
-                console.error(errorMessage);
-                alert('Falha ao acessar, '+ errorMessage);
-        });
+                catcherror(error);
+            });
+        
+        // firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+        // // When the user signs in with email and password.
+        // firebase.auth().signInWithEmailAndPassword(email, password)
+        //     .then(user => {
+        //         console.log(user);
+        //     // Get the user's ID token as it is needed to exchange for a session cookie.
+        //      return user.getIdToken().then(idToken => {
+        //             // Session login endpoint is queried and the session cookie is set.
+        //             // CSRF protection should be taken into account.
+        //             // ...
+        //         const csrfToken = getCookie('csrfToken')
+        //         return postIdTokenToSessionLogin('/sessionLogin', idToken, csrfToken);
+        //     });
+        //     // })
+        //     // .then(() => {
+        //     // // A page redirect would suffice as the persistence is set to NONE.
+        //     // return firebase.auth().signOut();
+        //     // })
+        //     // .then(() => {
+        //     // window.location.assign('home.html');
+        //     });
     });
+
+
+    $('.btn-authFacebook').click(authFacebook);
+
+    $('.btn-authTwitter').click(authTwitter);
+
+    $('.btn-authGoogle').click(authGoogle);
 })
- 
+
+function authFacebook(){
+    var provider = new firebase.auth.FacebookAuthProvider();
+    signIn(provider);
+}
+
+function authTwitter(){
+    var provider = new firebase.auth.TwitterAuthProvider();
+    signIn(provider);
+}
+
+function authGoogle(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+    signIn(provider);
+}
+
+function signIn(provider) {
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then(function (response) {
+            console.log(response);
+            // var token = response.credential.accessToken;
+            var uid = response.user.uid;
+            window.location = "home.html?userId=" + uid;
+        }).catch(function (error) {
+            catcherror(error);
+        });
+}
+
+function catcherror(error) {
+    alert(error.message);
+    console.log(error.code, error.message);
+}
+
